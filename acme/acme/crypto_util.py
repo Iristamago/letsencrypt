7e8486cdc3f3161927b0866c1845862bffd8297e
Jakub Warmuz <jakub@warmuz.org>
2016-04-10 10:27:27 +0000

    Add gen_csr and gen_pkey to acme.crypto_util.
    Implementation taken from https://github.com/kuba/simp_le.

--- acme/acme/crypto_util.py
+++ acme/acme/crypto_util.py
@@ -230,3 +230,56 @@ def gen_ss_cert(key, domains, not_before=None,
     cert.set_pubkey(key)
     cert.sign(key, "sha256")
     return cert
+
+
+def gen_pkey(bits):
+    """Generate a private key.
+
+    >>> gen_pkey(1024)
+    <OpenSSL.crypto.PKey object at 0x...>
+
+    Args:
+      bits: Bit size of the key.
+
+    Returns:
+      Freshly generated private key.
+    """
+    assert bits >= 1024  # XXX
+    pkey = OpenSSL.crypto.PKey()
+    pkey.generate_key(OpenSSL.crypto.TYPE_RSA, bits)
+    return pkey
+
+
+def gen_csr(pkey, domains, sig_hash='sha256'):
+    """Generate a CSR.
+
+    >>> crypto_util._pykopenssl_cert_or_req_san(
+    ...     gen_csr(gen_pkey(1024), [b'example.com', b'example.net']))
+    ['example.com', 'example.net']
+
+    Args:
+      pkey: Private key.
+      domains: List of domains included in the cert.
+      sig_hash: Hash used to sign the CSR.
+
+    Returns:
+      Generated CSR.
+    """
+    assert domains, 'Must provide one or more hostnames for the CSR.'
+    req = OpenSSL.crypto.X509Req()
+    req.add_extensions([
+        OpenSSL.crypto.X509Extension(
+            b'subjectAltName',
+            critical=False,
+            value=b', '.join(b'DNS:' + d for d in domains)
+        ),
+    ])
+    req.set_pubkey(pkey)
+
+    # pre-1.0.2 version of OpenSSL the generated CSR will contain a
+    # zero-length Version field which will cause some strict parsers
+    # (e.g. the one in Golang, used by Boulder) to fail.
+    req.set_version(2)
+
+    req.sign(pkey, sig_hash)
+    return req
